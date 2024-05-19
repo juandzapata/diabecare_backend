baf50706f825988fc9025e11dda5e8b257021e76
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-05-19 14:53:22 -0500

    feat: create login funtion

---
+++ database/db.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+Base = declarative_base()
+env = os.getenv("ENV","development")
+path = f'.env.{env}'
+dotenv_path = os.path.join('environments', path)
+load_dotenv(dotenv_path=dotenv_path)
+
+DATABASE_URL = os.getenv("DATABASE_CONECTION_STRING")
+engine = create_engine(DATABASE_URL, echo=True, future=True)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
