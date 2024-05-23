e117fe2c308fac181e5ad41089f85712556ebe38
juandzapata <juan.zapata22871@ucaldas.edu.co>
2024-05-23 17:18:59 -0500

    Creación de: router para Recomendación

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database.db import Base, engine
-from routers import user, account
+from routers import user, account, recomendacion
 import os
 
 Base.metadata.create_all(bind=engine)
@@ -11,9 +11,10 @@ CLIENT = os.getenv("CLIENT_URL")
 origins = ["https://localhost","http://localhost:8100"]
 app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,allow_methods=["*"],allow_headers=["*"])
 
+# Routers
 app.include_router(user.router, tags=["Usuarios"], prefix="/usuarios")
 app.include_router(account.router, tags=["Account"], prefix="/account")
-
+app.include_router(recomendacion.router, tags=["Recomendaciones"], prefix="/recomendaciones")
 
 
 @app.get("/")
--- routers/recomendacion.py
+++ routers/recomendacion.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from database.db import get_db
-#from schemas.appointment import AppointmentOut, AppointmentRegister, GetAppointmentByDoctorIdByUser
-#from services.appointment import get_appointments, get_appointments_by_user_id, create_appointment_function, get_available_hours_by_date, patch_appointment_state, post_appointment
-from typing import List
+from schemas.recomendacion import RecomendacionCreate, RecomendacionOut 
+from services.recomendacion import post_recomendacion
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
+
+router = APIRouter()
+
+@router.post("/recomendacion", response_model=RecomendacionOut, summary="Crear una recomendación")
+async def create_recomendacion(recomendacion: RecomendacionCreate, db: Session = Depends(get_db)) -> RecomendacionOut:
+    recomendacion_creada: RecomendacionOut = post_recomendacion(recomendacion, db)
+    if recomendacion_creada is None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = f'Recomendacion {recomendacion.titulo} already exists'
+        )
+    return JSONResponse(
+        status_code = status.HTTP_201_CREATED,
+        content = {'data': jsonable_encoder(recomendacion_creada)}
+   )
