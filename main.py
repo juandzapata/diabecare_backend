55dd8758b0303694e0f8e6267b7c5edd6fb03057
Luis Andres Martinez Estremor <luis.martineze@opitech.com.co>
2024-05-25 02:25:02 -0500

    Refactor routers and update imports for consistency

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import token
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from database.db import Base, engine
-from routers import user, account, recomendacion, planes_personalizados, paciente, notification
+from routers import user, account, recomendation, personalized_planes, patient, notification
 import firebase_admin
 from firebase_admin import credentials
 from routers import patient, personalized_planes, recomendation, user, account, file
@@ -36,11 +36,8 @@ app.add_middleware(CORSMiddleware,allow_origins=origins,allow_credentials=True,a
 
 # Routers
 app.include_router(user.router, tags=["Usuarios"], prefix="/users")
-app.include_router(account.router, tags=["Account"], prefix="/account")
-app.include_router(recomendacion.router, tags=["Recomendaciones"], prefix="/recomendaciones")
-app.include_router(planes_personalizados.router, tags=["Planes Personalizados"], prefix="/planes_personalizados")
-app.include_router(paciente.router, tags=["Pacientes"], prefix="/pacientes")
-app.include_router(notification.router, tags=["Notificaciones"], prefix="/notificaciones")
+app.include_router(patient.router, tags=["Pacientes"], prefix="/pacientes")
+app.include_router(notification.router, tags=["Notifications"], prefix="/notificaciones")
 app.include_router(recomendation.router, tags=["Recomendations"], prefix="/recomendations")
 app.include_router(personalized_planes.router, tags=["Personalized Planes"], prefix="/personalized_planes")
 app.include_router(patient.router, tags=["Patients"], prefix="/patients")
